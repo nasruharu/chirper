fbe0ec19b72b8e6b6a8052ea0fc6f3205cf4f5e2
hassan <hassanashrallah.ar@gmail.com>
2026-05-29 21:53:28 +0700

    basic authentication

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -26,23 +26,21 @@ class ChirpController extends Controller
             'message.max' => 'Chirps must be 255 characters or less.',
         ]);
 
-        \App\Models\Chirp::create([
-            'message' => $validated['message'],
-            'user_id' => null,
-        ]);
+        auth()->user()->chirps()->create($validated);
 
         return redirect('/')->with('success', 'Your chirp has been posted!');
     }
 
     public function edit(Chirp $chirp)
     {
-        // We'll add authorization in lesson 11
+        $this->authorize('update', $chirp);
+
         return view('chirps.edit', compact('chirp'));
     }
 
     public function update(Request $request, Chirp $chirp)
     {
-        // $this->authorize('update', $chirp);
+        $this->authorize('update', $chirp);
 
         // Validate
         $validated = $request->validate([
@@ -57,6 +55,8 @@ class ChirpController extends Controller
 
     public function destroy(Chirp $chirp)
     {
+        $this->authorize('delete', $chirp);
+            
         $chirp->delete();
 
         return redirect('/')->with('success', 'Chirp deleted!');
